e3aa48e7cee816e2ef663ad23a7240004f74eeae
西村　祐樹 <nishimura.yuki@synergy101.jp>
2016-09-18 16:32:36 +0900

    テストスクリプトを更新。

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /var/sites/jp.ecumerun.com/webapps/current
+cd /var/sites/test
 bundle install --path=./vendor/bundle
-#~/.rbenv/shims/bundle install --path=/var/sites/jp.ecumerun.com/webapps/current/vendor/bundle
+#~/.rbenv/shims/bundle install --path=/var/sites/test
 
 bundle exec rake test
 #~/.rbenv/shims/bundle exec rake test
