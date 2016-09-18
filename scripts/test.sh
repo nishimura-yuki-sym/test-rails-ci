6fce2b8303b92c6dc14a380a3717d1f7852533f9
西村　祐樹 <nishimura.yuki@synergy101.jp>
2016-09-18 16:40:55 +0900

    設定更新。

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /var/sites/test
-bundle install --path=./vendor/bundle
+~/.rbenv/shims/bundle install --path=./vendor/bundle
 #~/.rbenv/shims/bundle install --path=/var/sites/test
 
-bundle exec rake test
+~/.rbenv/shims/bundle exec rake test
 #~/.rbenv/shims/bundle exec rake test
